551d83e3a18f4cf7266de8baa0c770d261c335c0
xd_pxrt <xdpxrt@yandex.ru>
2024-04-16 15:28:59 +0300

    feat: add client

---
+++ stats-service/stats-client/src/main/java/ru/yandex/practicum/ClientConfig.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ClientConfig {
+    @Value("${stats-server.url}")
+    private String baseUrl;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
--- stats-service/stats-client/src/main/java/ru/yandex/practicum/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/yandex/practicum/StatsClient.java
@@ -1,7 +1,48 @@
 package ru.yandex.practicum;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class StatsClient {
+
+    private final WebClient webClient;
+
+    public EndpointHit addStat(EndpointHit endpointHit) {
+        String uri = "/hits";
+        log.info("Request on uri {} body {}", uri, endpointHit);
+        return webClient
+                .post()
+                .uri(uri)
+                .bodyValue(endpointHit)
+                .retrieve()
+                .bodyToMono(EndpointHit.class)
+                .block();
+    }
+
+    public List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        String request = String.format("/stats?start=%s&end=%s&uris=%s&unique=%s",
+                encode(String.valueOf(start)), encode(String.valueOf(end)), uris, unique);
+        log.info("Request on uri {}", request);
+        return webClient
+                .get()
+                .uri(request)
+                .retrieve()
+                .bodyToFlux(ViewStats.class)
+                .collectList()
+                .block();
+    }
+
+    private String encode(String value) {
+        return URLEncoder.encode(value, StandardCharsets.UTF_8);
+    }
 }
